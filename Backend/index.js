3b9fcb04a73369d63b578ab6bdff767fcc1b1259
Pratham Patel <ppratham134patel@gmail.com>
2025-11-12 01:23:13 +0530

    Fixed CORS for production

--- Backend/index.js
+++ Backend/index.js
@@ -14,11 +14,13 @@ import { server, app } from "./config/socket.js";
 const port = process.env.PORT || 3232;
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin:[ 
+      process.env.CLIENT_URL,
+      "http://localhost:5173"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
-
 
 // middleware
 app.use(cors(corsOptions));
